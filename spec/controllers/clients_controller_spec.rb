150a7561f3f0c4c5eafb8a9e3279956d3c856466
Leonel Samayoa <leonel.e.samayoa@gmail.com>
2014-10-09 14:22:25 +0000

    Refactored tests to use the included FactoryGirl methods.

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ClientsController, :type => :controller do
 
   let(:valid_attributes) {
-    FactoryGirl.attributes_for(:client)
+    attributes_for(:client)
   }
 
   let(:invalid_attributes) {
@@ -22,19 +22,19 @@ RSpec.describe ClientsController, :type => :controller do
 
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      @user = FactoryGirl.create(:user)
+      @user = create(:user)
       sign_in @user
     end
 
     describe "GET index" do
       it "assigns all logged user clients as @clients" do
-        client = FactoryGirl.create(:client, user: @user)
+        client = create(:client, user: @user)
         get :index, {}, valid_session
         expect(assigns(:clients)).to eq([client])
       end
 
       it "should not show other user's clients" do
-        client = FactoryGirl.create(:client)
+        client = create(:client)
         get :index, {}, valid_session
         expect(assigns(:clients)).to eq([])
       end
@@ -42,13 +42,13 @@ RSpec.describe ClientsController, :type => :controller do
 
     describe "GET show" do
       it "assigns the requested client as @client" do
-        client = FactoryGirl.create(:client, user: @user)
+        client = create(:client, user: @user)
         get :show, {:id => client.to_param}, valid_session
         expect(assigns(:client)).to eq(client)
       end
 
       it "should not show other user's clients" do
-        client = FactoryGirl.create(:client)
+        client = create(:client)
         bypass_rescue
         expect{
           get :show, {:id => client.to_param}, valid_session
@@ -65,13 +65,13 @@ RSpec.describe ClientsController, :type => :controller do
 
     describe "GET edit" do
       it "assigns the requested client as @client" do
-        client = FactoryGirl.create(:client, user: @user)
+        client = create(:client, user: @user)
         get :edit, {:id => client.to_param}, valid_session
         expect(assigns(:client)).to eq(client)
       end
 
       it "should redirect to clients if client do not belong to the logged user" do
-        client = FactoryGirl.create(:client)
+        client = create(:client)
         bypass_rescue
         expect{
           get :edit, {:id => client.to_param}, valid_session
@@ -121,11 +121,11 @@ RSpec.describe ClientsController, :type => :controller do
     describe "PUT update" do
       describe "with valid params" do
         let(:new_attributes) {
-          FactoryGirl.attributes_for(:client, first_name: "Changed")
+          attributes_for(:client, first_name: "Changed")
         }
 
         it "should not alllow to update other user's client" do
-          client = FactoryGirl.create(:client)
+          client = create(:client)
           bypass_rescue
           expect{
             put :update, {:id => client.to_param, :client => new_attributes}, valid_session
@@ -133,19 +133,19 @@ RSpec.describe ClientsController, :type => :controller do
         end
 
         it "updates the requested client" do
-          client = FactoryGirl.create(:client, user: @user)
+          client = create(:client, user: @user)
           put :update, {:id => client.to_param, :client => new_attributes}, valid_session
           expect(client.reload.first_name).to eq "Changed"
         end
 
         it "assigns the requested client as @client" do
-          client = FactoryGirl.create(:client, user: @user)
+          client = create(:client, user: @user)
           put :update, {:id => client.to_param, :client => valid_attributes}, valid_session
           expect(assigns(:client)).to eq(client)
         end
 
         it "redirects to the client" do
-          client = FactoryGirl.create(:client, user: @user)
+          client = create(:client, user: @user)
           put :update, {:id => client.to_param, :client => valid_attributes}, valid_session
           expect(response).to redirect_to(client)
         end
@@ -153,13 +153,13 @@ RSpec.describe ClientsController, :type => :controller do
 
       describe "with invalid params" do
         it "assigns the client as @client" do
-          client = FactoryGirl.create(:client, user: @user)
+          client = create(:client, user: @user)
           put :update, {:id => client.to_param, :client => invalid_attributes}, valid_session
           expect(assigns(:client)).to eq(client)
         end
 
         it "redirects back to the client detail page" do
-          client = FactoryGirl.create(:client, user: @user)
+          client = create(:client, user: @user)
           put :update, {:id => client.to_param, :client => invalid_attributes}, valid_session
           expect(response).to redirect_to(client_path(client))
         end
@@ -168,14 +168,14 @@ RSpec.describe ClientsController, :type => :controller do
 
     describe "DELETE destroy" do
       it "destroys the requested client" do
-        client = Client.create! valid_attributes
+        client = create(:client, user: @user)
         expect {
           delete :destroy, {:id => client.to_param}, valid_session
         }.to change(Client, :count).by(-1)
       end
 
       it "redirects to the clients list" do
-        client = Client.create! valid_attributes
+        client = create(:client, user: @user)
         delete :destroy, {:id => client.to_param}, valid_session
         expect(response).to redirect_to(clients_url)
       end
