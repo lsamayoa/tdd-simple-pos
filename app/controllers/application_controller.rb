70dfe6e635feddc6a9754e1d8c6061ed60fc1b86
Leonel Samayoa <leonel.e.samayoa@gmail.com>
2014-10-09 14:15:55 +0000

    Added strong parameters missing handling to the application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   respond_to :html, :json, :xml
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from ActionController::ParameterMissing, with: :parameters_missing
 
   private
     def user_not_authorized(exception)
@@ -13,6 +14,17 @@ class ApplicationController < ActionController::Base
 
       flash[:error] = I18n.t "pundit.#{policy_name}.#{exception.query}",
         default: 'You cannot perform this action.'
+      redirect_to_last_visited_path
+    end
+
+    def parameters_missing(exception)
+      flash[:error] = I18n.t "parameter_missing",
+        default: 'There is a parameter missing.',
+        param: exception.param
+      redirect_to_last_visited_path
+    end
+
+    def redirect_to_last_visited_path
       redirect_to(request.referrer || root_path)
     end
 end
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,31 +2,59 @@ require 'rails_helper'
 
 RSpec.describe ApplicationController, :type => :controller do
   controller do
-    def index
+    def not_authorized_error
       error = Pundit::NotAuthorizedError.new 
       error.policy = ApplicationPolicy.new(nil, nil)
       error.query = "index?"
       raise error
+    end
+    def param_missing
+      raise ActionController::ParameterMissing.new("parameter")
+    end
+  end
+
+  before do
+    routes.draw do
+      get "not_authorized_error" => "anonymous#not_authorized_error"
+      get "param_missing" => "anonymous#param_missing" 
+    end
+  end
+
+  describe "Strong parameter handling" do
+
+    it "should rescue from ActionController::ParameterMissing" do
+      expect{
+        get :param_missing
+      }.not_to raise_error
+    end
+
+    it "should should set the error flash message based on the parameter that is missing" do
+      get :param_missing
+      expect(flash[:error]).to eq "parameter is missing."
+    end
+
+    it "should redirect to the last visited path or back to the root" do
+      get :param_missing
+      expect(response).to redirect_to(request.referrer || root_path)
     end
   end
 
   describe "Authorization Error Handling" do
     it "should rescue from Pundit::NotAuthorizedError" do
       expect{
-        get :index
+        get :not_authorized_error
       }.not_to raise_error
     end
 
     it "should should set the error flash message based on the policy that threw the error" do
-      get :index
+      get :not_authorized_error
       expect(flash[:error]).to eq "You cannot perform the index action."
     end
 
     it "should redirect to the last visited path or back to the root" do
-      get :index
+      get :not_authorized_error
       expect(response).to redirect_to(request.referrer || root_path)
     end
-
   end
 
 end
